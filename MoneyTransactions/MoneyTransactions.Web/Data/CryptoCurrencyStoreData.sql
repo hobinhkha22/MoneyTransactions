dfebbd6104e7b5f0dc2bf62305ab60fb2cfdf912
hobinhkha22@gmail.com <hobinhkha22@gmail.com>
2019-11-01 13:00:19 +0700

    [master] Add Cryptocurrencydata sql

---
+++ MoneyTransactions/MoneyTransactions.Web/Data/CryptoCurrencyStoreData.sql
@@ -0,0 +1,8 @@
+USE MoneyTransactions
+GO
+
+INSERT INTO CryptocurrencyStore([CryptocurrencyStoreID], [Description],[MoneyType]) VALUES ('f8600eba-b98b-4f2e-827e-aa5af920f83b', 'Bitcoin', 'BTC')
+INSERT INTO CryptocurrencyStore([CryptocurrencyStoreID], [Description],[MoneyType]) VALUES ('0e2420f0-f4f4-406e-bca7-49bf3fe1ebe9', 'Ripple', 'XRP')
+INSERT INTO CryptocurrencyStore([CryptocurrencyStoreID], [Description],[MoneyType]) VALUES ('e41f4848-5008-443c-bfd7-4a306c809003', 'Ethereum', 'ETH')
+
+GO
