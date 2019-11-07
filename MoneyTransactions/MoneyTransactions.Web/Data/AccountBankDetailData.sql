78b8cc49e0c73b08306a37336335b35c834bf60c
hobinhkha22@gmail.com <hobinhkha22@gmail.com>
2019-11-07 22:27:56 +0700

    [master] Transaction temp 2

---
+++ MoneyTransactions/MoneyTransactions.Web/Data/AccountBankDetailData.sql
@@ -0,0 +1,8 @@
+use MoneyTransactions
+go
+
+INSERT INTO AccountBankDetail([AccountBankDetailID], [AccountID], [AccountNumber], [BankID], [CardNumber], [ExpiredDate]) VALUES('dd03adfe-e507-46e0-8a99-7dc3778d50ff', '', '2301685749', '', '4815336355841003', '2025-07-23 16:22:30.21245Z')
+INSERT INTO AccountBankDetail([AccountBankDetailID], [AccountID], [AccountNumber], [BankID], [CardNumber], [ExpiredDate]) VALUES('974f3169-b0da-40a3-8f0d-d2110464ecab', '', '7316290485', '', '4443761071599017', '2024-06-25 10:15:30.13245Z')
+INSERT INTO AccountBankDetail([AccountBankDetailID], [AccountID], [AccountNumber], [BankID], [CardNumber], [ExpiredDate]) VALUES('3313f949-329c-4ca0-b677-2a7eb0cac22a', '', '6247081359', '', '4774058369904978', '2025-08-02 15:50:30.21345Z')
+INSERT INTO AccountBankDetail([AccountBankDetailID], [AccountID], [AccountNumber], [BankID], [CardNumber], [ExpiredDate]) VALUES('fac38398-2017-4720-8d5a-f9b407948130', '', '6754032918', '', '4265822284714929', '2026-05-17 08:39:30.25845Z')
+INSERT INTO AccountBankDetail([AccountBankDetailID], [AccountID], [AccountNumber], [BankID], [CardNumber], [ExpiredDate]) VALUES('e62035eb-1d02-45d6-8704-14bffbc5f1b4', '', '5437189026', '', '4144442044944026', '2026-03-03 48:30:30.19085Z')
---
+++ MoneyTransactions/MoneyTransactions.Web/Data/BankData.sql
@@ -0,0 +1,10 @@
+USE MoneyTransactions
+GO
+
+INSERT INTO Bank([BankID], [BankName]) VALUES('a2e6fde5-0fa9-44fe-9357-2eb499f077bc', 'Agribank')
+INSERT INTO Bank([BankID], [BankName]) VALUES('9550a110-be22-45b0-a3d4-c4f47036ccc1', 'TPBank')
+INSERT INTO Bank([BankID], [BankName]) VALUES('69cf0098-491e-4f25-9589-dfb778843cc4', 'VietcomBank')
+INSERT INTO Bank([BankID], [BankName]) VALUES('952a44f7-2b58-4d5f-a868-636356a16229', 'ACB')
+INSERT INTO Bank([BankID], [BankName]) VALUES('4897f257-71cd-48e5-8b6a-927434b66164', 'SacomBank')
+
+GO
