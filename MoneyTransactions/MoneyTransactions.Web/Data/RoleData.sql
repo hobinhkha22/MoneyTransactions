6463eacdc44b828c7806fd2c13b239467f897592
hobinhkha22@gmail.com <hobinhkha22@gmail.com>
2019-11-14 01:04:36 +0700

    [master] Update data template

---
+++ MoneyTransactions/MoneyTransactions.Web/Data/RoleData.sql
@@ -0,0 +1,6 @@
+﻿USE [MoneyTransaction]
+GO
+INSERT INTO Role([RoleID], [RoleName], [Description]) VALUES('0c3ab0cc-6cac-4235-a9c0-94aedc2af6fa', 'User', 'User role')
+INSERT INTO Role([RoleID], [RoleName], [Description]) VALUES('186b054d-f8dc-4617-b955-d4240aefd9e4', 'Admin', 'Admin role')
+
+GO
