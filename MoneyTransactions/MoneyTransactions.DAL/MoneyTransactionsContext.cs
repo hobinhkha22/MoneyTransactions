6c57e68b9944b7d75b52cb32249291945aa21433
Thanh Le Tat <tatthanh.uz@gmail.com>
2019-11-14 00:45:26 +0700

    Update Code

---
+++ MoneyTransactions/MoneyTransactions.DAL/MoneyTransactionsContext.cs
@@ -0,0 +1,34 @@
+﻿using MoneyTransactions.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransactions.DAL
+{
+    public class MoneyTransactionsContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountBankDetail> AccountBankDetails { get; set; }
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<CryptocurrencyStore> CryptocurrencyStores { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+
+        public MoneyTransactionsContext(string connectionString):base(connectionString)
+        {
+            Database.Connection.ConnectionString = connectionString;
+            Database.SetInitializer(new MoneyTransactionsDBInitializer());
+            Database.Initialize(false);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MoneyTransactions/MoneyTransactions.DAL/MoneyTransactionsDBInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransactions.DAL
+{
+    public class MoneyTransactionsDBInitializer : DropCreateDatabaseIfModelChanges<MoneyTransactionsContext>
+    {
+        protected override void Seed(MoneyTransactionsContext context)
+        {
+            base.Seed(context);
+
+        }
+
+    }
+}
